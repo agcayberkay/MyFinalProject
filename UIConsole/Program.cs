a6bd63d341646622f4a1c087c131728bddb36b32
berkay <dev1>
2023-12-27 14:43:08 +0300

    EntityFramework güncellendi.

--- UIConsole/Program.cs
+++ UIConsole/Program.cs
@@ -9,8 +9,8 @@ namespace UIConsole
     {
         static void Main(string[] args)
         {
-            //ProductListName();
-            //CategoryListName();
+            ProductListName();
+            CategoryListName();
 
         }
 
